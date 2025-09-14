0d30bd05ab02f9b7e59c0c37f34e238694912438
mitanshu <mitanshug2004@gmail.com>
2025-09-15 00:49:54 +0530

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,15 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.tsx"
 import "./styles/global.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode> {/* We've removed this wrapper */}
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <App />
-    </BrowserRouter>
-  // </React.StrictMode>
+  <BrowserRouter
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
+    <App />
+  </BrowserRouter>
 )
